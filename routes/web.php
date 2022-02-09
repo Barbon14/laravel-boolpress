53bac665594c093a362e1832ade5c60bcbe768ef
BarbonFisso <barbieri.mat@hotmail.it>
2022-02-09 15:59:08 +0100

    Routes refactoring

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,15 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'GuestController@home')->name('home');
-Route::get('/list', 'GuestController@viewPostList')-> name('postList');
-Route::get('/list/post/create', 'GuestController@create') ->name('create');
-Route::post('/list/post/store', 'GuestController@store')-> name('store');
-Route::get('/list/post/show/{id}', 'GuestController@show')->name('show');
+Route::get('/posts', 'GuestController@viewPostList')-> name('postList');
+Route::get('/posts/show/{id}', 'GuestController@show')->name('show');
 
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+Route::middleware('auth') -> group(function() {
+    Route::get('/posts/create', 'GuestController@create') ->name('create');
+    Route::post('/posts/store', 'GuestController@store')-> name('store');
+});
